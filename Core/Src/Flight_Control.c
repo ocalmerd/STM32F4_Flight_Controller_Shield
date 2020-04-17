9bc4b4f0c4cf4d779e327397a268a0ffe538fbac
Mert <ocalmerd@gmail.com>
2020-04-17 20:54:43 +0300

    PID update
    PID parameters update

--- Core/Src/Flight_Control.c
+++ Core/Src/Flight_Control.c
@@ -11,20 +11,20 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //PID gain and limit settings
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-float pid_p_gain_roll = 1.7;               	//Gain setting for the roll P-controller (1.4) 			- (1.6)
-float pid_i_gain_roll = 0.006;              	//Gain setting for the roll I-controller (0.05)		- (0.005)
-float pid_d_gain_roll = 60;                	//Gain setting for the roll D-controller (15)			- (50)
-int pid_max_roll = 400;                   	//Maximum output of the PID-controller (+/-) (400)
+float pid_p_gain_roll = 1.6;               	//Gain setting for the roll P-controller 		- (1.6)
+float pid_i_gain_roll = 0.007;              //Gain setting for the roll I-controller 		- (0.007)
+float pid_d_gain_roll = 100;                //Gain setting for the roll D-controller 		- (100)
+int pid_max_roll = 400;                   	//Maximum output of the PID-controller (+/-) 	- (400)
 
-float pid_p_gain_pitch = 1.7;    			//Gain setting for the pitch P-controller (1.4)			- (1.6)
-float pid_i_gain_pitch = 0.006;  			//Gain setting for the pitch I-controller (0.05)		- (0.005)
-float pid_d_gain_pitch = 60;  				//Gain setting for the pitch D-controller (15)			- (50)
-int pid_max_pitch = 400;          			//Maximum output of the PID-controller (+/-) (400)
+float pid_p_gain_pitch = 1.6;    			//Gain setting for the pitch P-controller		- (1.6)
+float pid_i_gain_pitch = 0.007;  			//Gain setting for the pitch I-controller 		- (0.007)
+float pid_d_gain_pitch = 100;  				//Gain setting for the pitch D-controller 		- (100)
+int pid_max_pitch = 400;          			//Maximum output of the PID-controller (+/-)    - (400)
 
-float pid_p_gain_yaw = 4.0;                	//Gain setting for the pitch P-controller (4.0)
-float pid_i_gain_yaw = 0.02;               	//Gain setting for the pitch I-controller (0.02)
-float pid_d_gain_yaw = 0.0;                	//Gain setting for the pitch D-controller (0)
-int pid_max_yaw = 400;                     	//Maximum output of the PID-controller (+/-) (400)
+float pid_p_gain_yaw = 4.0;                	//Gain setting for the pitch P-controller 		- (4.0)
+float pid_i_gain_yaw = 0.02;               	//Gain setting for the pitch I-controller  		- (0.02)
+float pid_d_gain_yaw = 0.0;                	//Gain setting for the pitch D-controller   	- (0)
+int pid_max_yaw = 400;                     	//Maximum output of the PID-controller (+/-) 	- (400)
 
 float pid_error_temp;
 float pid_i_mem_roll, pid_roll_setpoint, gyro_roll_input, pid_output_roll, pid_last_roll_d_error;
@@ -212,7 +212,7 @@ void Calculate_PID( void )
 
 	pid_output_roll = pid_p_gain_roll * pid_error_temp + pid_i_mem_roll + pid_d_gain_roll * (pid_error_temp - pid_last_roll_d_error);
 	if		( pid_output_roll > pid_max_roll )			{	pid_output_roll = pid_max_roll;	}
-	else if	( pid_output_roll < pid_max_roll * -1)		{	pid_output_roll = pid_max_roll * -1;	}
+	else if	( pid_output_roll < pid_max_roll * -1 )		{	pid_output_roll = pid_max_roll * -1;	}
 
 	pid_last_roll_d_error = pid_error_temp;
 
